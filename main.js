7581acc587d1189675dfe9271377648dc5081b3f
prog5 <prog5@it-delta.ru>
2025-02-06 13:17:12 +0300

    Added notifications: Implement timer polling API every 10 minutes, openAtLogin

--- main.js
+++ main.js
@@ -1,11 +1,17 @@
 // main.js
 
 // Modules to control application life and create native browser window
-const { app, BrowserWindow, Tray, Menu, nativeImage } = require('electron')
+const { app, BrowserWindow, Tray, Menu, nativeImage, session, Notification } = require('electron')
 const path = require('node:path')
+
+app.setLoginItemSettings({
+    openAtLogin: true, //Start app when login to OS
+});
+
 
 let tray, mainWindow
 
+const appUrl = 'https://staging.cloudcollect.dk/';
 const createWindow = () => {
     // Create the browser window.
     mainWindow = new BrowserWindow({
@@ -16,15 +22,61 @@ const createWindow = () => {
         }
     })
 
-    // and load the index.html of the app.
-    mainWindow.loadFile('index.html')
+    // mainWindow.on('close', (event) => {
+    //     event.preventDefault();
+    //     mainWindow.hide(); // Just hide window
+    // });
+
+    // and load the SPA
+    mainWindow.loadURL(appUrl)
+
+    // Fetch notifications every 10 minutes
+    setInterval(fetchNotifications, 10 * 60 * 1000);
 }
+
+async function fetchNotifications() {
+    try {
+        // Get cookies from default app session
+        const cookies = await session.defaultSession.cookies.get({url: appUrl});
+        const cookieString = cookies.map(c => `${c.name}=${c.value}`).join('; ');
+
+        // Fetch notifications
+        const response = await fetch('https://staging.cloudcollect.dk/todo/company-todos', {
+            method: 'GET',
+            headers: {
+                'Cookie': cookieString // needed for auth
+            },
+        });
+
+        const notifications = await response.json();
+        if (notifications.length > 0) {
+            showNotification(notifications[0].text, notifications[0].created_at);
+        }
+
+    } catch (error) {
+        console.error('fetchNotifications got error:', error);
+    }
+}
+
+function showNotification(title, body) {
+    //Create OS desktop notifications
+    const notification = new Notification({ title, body });
+    notification.show();
+
+    notification.on('click', () => {
+        if (mainWindow) {
+            mainWindow.show(); // Show window
+            mainWindow.focus();
+        }
+    });
+}
+
 
 const createTray = () => {
     // Make the application minimize to Tray
 
     const icon = nativeImage.createFromPath('assets/icon.ico') //TODO change to ico for windows, 16×16, 32×32, 48×48, 64×64 и 256×256 in one file
-    console.log('Иконка пуста?', icon.isEmpty())
+    console.log('Icon is empty?', icon.isEmpty())
 
     tray = new Tray(icon)
 
